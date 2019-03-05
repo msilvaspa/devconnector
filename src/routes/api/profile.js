f960428497827101f218c24ed7aaa36096d38f5d
Mateus Alves <msilva.spa@hotmail.com>
2019-03-04 23:01:15 -0300

    create & update profile route

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -22,4 +22,51 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  const profileFields = {};
+  profileFields.user = req.user.id;
+
+  const profileInfo = [
+    'handle',
+    'company',
+    'website',
+    'location',
+    'bio',
+    'status',
+    'githubusername',
+  ];
+
+  profileInfo.forEach((info) => {
+    if (req.body[info]) profileFields[info] = req.body[info];
+  });
+
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',');
+  }
+
+  profileFields.social = {};
+  const socialArray = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
+
+  socialArray.forEach((social) => {
+    if (req.body[social]) profileFields.social[social] = req.body[social];
+  });
+
+  Profile.findOne({ user: req.user.id }).then((profile) => {
+    if (profile) {
+      Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true }).then(
+        profileUpdated => res.json(profileUpdated),
+      );
+    } else {
+      Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+        if (profile) {
+          errors.handle = 'that handle already exists';
+          res.status(400).json(errors);
+        }
+        new Profile(profileFields).save().then(profile => res.json(profile));
+      });
+    }
+  });
+});
+
 module.exports = router;
