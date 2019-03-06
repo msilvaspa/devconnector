255dc135d7df9c9f07e83b22031b42c2d3fe6819
Mateus MAS. Alves da Silva <msilva@Admins-Mac-mini.local>
2019-03-06 12:43:40 -0300

    more profile api routes

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -24,6 +24,50 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+router.get('/all', (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then((profiles) => {
+      if (!profiles) {
+        errors.niprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+});
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+});
+
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errors, isValid } = validateProfileInput(req.body);
 
@@ -31,22 +75,12 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 
   const profileFields = {};
   profileFields.user = req.user.id;
-  const profileInfo = [
-    'handle',
-    'company',
-    'website',
-    'location',
-    'bio',
-    'status',
-    'githubusername',
-  ];
-  profileInfo.forEach((info) => {
+  const profInfo = ['handle', 'company', 'website', 'location', 'bio', 'status', 'githubusername'];
+  profInfo.forEach((info) => {
     if (req.body[info]) profileFields[info] = req.body[info];
   });
 
-  if (typeof req.body.skills !== 'undefined') {
-    profileFields.skills = req.body.skills.split(',');
-  }
+  if (typeof req.body.skills !== 'undefined') profileFields.skills = req.body.skills.split(',');
 
   profileFields.social = {};
   const socialArray = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
@@ -56,17 +90,17 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 
   Profile.findOne({ user: req.user.id }).then((profile) => {
     if (profile) {
-      Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true }).then(
-        profileUpdated => res.json(profileUpdated),
-      );
+      Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+        .then(profileUpdated => res.json(profileUpdated));
     } else {
-      Profile.findOne({ handle: profileFields.handle }).then((profile) => {
-        if (profile) {
-          errors.handle = 'that handle already exists';
-          res.status(400).json(errors);
-        }
-        new Profile(profileFields).save().then(profile => res.json(profile));
-      });
+      Profile.findOne({ handle: profileFields.handle })
+        .then((profile) => {
+          if (profile) {
+            errors.handle = 'that handle already exists';
+            res.status(400).json(errors);
+          }
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
     }
   });
 });
